a97d874d8ec4d597814dac9358f729db00571223
Mihai Popescu <mihai.ppscu@gmail.com>
2025-05-21 13:49:23 +0300

    fix: main

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,15 @@ import subprocess
 import time
 import uuid
 import boto3
+import cv2
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+import ffmpegcv
 import modal
+import numpy as np
 from pydantic import BaseModel
 from google import genai
+from tqdm import tqdm
 import whisperx
 
 
@@ -47,6 +51,98 @@ def create_vertical_video(tracks, scores, pyframes_path, pyavi_path, audio_path,
     flist = glob.glob(os.path.join(pyframes_path, "*.jpg"))
     flist.sort()
 
+    faces = [[] for _ in range(len(flist))]
+
+    for tidx, track in enumerate(tracks):
+        score_array = scores[tidx]
+        for fidx, frame in enumerate(track["track"]["frame"].tolist()):
+            slice_start = max(fidx - 30, 0)
+            slice_end = min(fidx + 30, len(score_array))
+            score_slice = score_array[slice_start:slice_end]
+            avg_score = float(np.mean(score_slice)
+                              if len(score_slice) > 0 else 0)
+            
+            faces[frame].append(
+                {'track': tidx, 'score': avg_score, 's': track['proc_track']["s"][fidx], 'x': track['proc_track']["x"][fidx], 'y': track['proc_track']["y"][fidx]})
+            
+    temp_video_path = os.path.join(pyavi_path, "video_only.mp4")
+    vout = None
+    for fidx, fname in tqdm(enumerate(flist), total=len(flist), desc="Creating vertical video"):
+        img = cv2.imread(fname)
+        if img is None:
+            continue
+
+        current_faces = faces[fidx]
+
+        max_score_face = max(
+            current_faces, key=lambda face: face['score']) if current_faces else None
+
+        if max_score_face and max_score_face['score'] < 0:
+            max_score_face = None
+
+        if vout is None:
+            vout = ffmpegcv.VideoWriterNV(
+                file=temp_video_path,
+                codec=None,
+                fps=framerate,
+                resize=(target_width, target_height)
+            )
+
+        if max_score_face:
+            mode = "crop"
+        else:
+            mode = "resize"
+
+        if mode == "resize":
+            scale = target_width / img.shape[1]
+            resized_height = int(img.shape[0] * scale)
+            resized_image = cv2.resize(
+                img, (target_width, resized_height), interpolation=cv2.INTER_AREA)
+
+            scale_for_bg = max(
+                target_width / img.shape[1], target_height / img.shape[0])
+            bg_width = int(img.shape[1] * scale_for_bg)
+            bg_heigth = int(img.shape[0] * scale_for_bg)
+
+            blurred_background = cv2.resize(img, (bg_width, bg_heigth))
+            blurred_background = cv2.GaussianBlur(
+                blurred_background, (121, 121), 0)
+
+            crop_x = (bg_width - target_width) // 2
+            crop_y = (bg_heigth - target_height) // 2
+            blurred_background = blurred_background[crop_y:crop_y +
+                                                    target_height, crop_x:crop_x + target_width]
+
+            center_y = (target_height - resized_height) // 2
+            blurred_background[center_y:center_y +
+                               resized_height, :] = resized_image
+
+            vout.write(blurred_background)
+
+        elif mode == "crop":
+            scale = target_height / img.shape[0]
+            resized_image = cv2.resize(
+                img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+            frame_width = resized_image.shape[1]
+
+            center_x = int(
+                max_score_face["x"] * scale if max_score_face else frame_width // 2)
+            top_x = max(min(center_x - target_width // 2,
+                        frame_width - target_width), 0)
+
+            image_cropped = resized_image[0:target_height,
+                                          top_x:top_x + target_width]
+
+            vout.write(image_cropped)
+
+    if vout:
+        vout.release()
+
+    ffmpeg_command = (f"ffmpeg -y -i {temp_video_path} -i {audio_path} "
+                      f"-c:v h264 -preset fast -crf 23 -c:a aac -b:a 128k "
+                      f"{output_path}")
+    subprocess.run(ffmpeg_command, shell=True, check=True, text=True)
+
 
 def process_clip(base_dir: str, original_video_path: str, s3_key: str, start_time: float, end_time: float, clip_index: int, transcript_segments: list):
     clip_name = f"clip_{clip_index}"
@@ -56,7 +152,7 @@ def process_clip(base_dir: str, original_video_path: str, s3_key: str, start_tim
     print(f"Output s3 key: {output_s3_key}")
 
     clip_dir = base_dir / clip_name
-    clip_dir.mkdir(parents=True, exists_ok=True)
+    clip_dir.mkdir(parents=True, exist_ok=True)
 
     # Segment path: original clip from strat to end
     clip_segment_path = clip_dir / f"{clip_name}_segment.mp4"
@@ -114,6 +210,10 @@ def process_clip(base_dir: str, original_video_path: str, s3_key: str, start_tim
 
     print(
         f"Clip {clip_index} vertical video creation time: {cvv_end_time - cvv_start_time:.2f} seconds")
+    
+    s3_client = boto3.client("s3")
+    s3_client.upload_file(
+        subtitle_output_path, "iaiai-podcast-clipper", output_s3_key)
 
 
 @app.cls(gpu="L40S", timeout=900, retries=0, scaledown_window=20, secrets=[modal.Secret.from_name("ai-podcast-clipper-secret")], volumes={mount_path: volume})
@@ -234,9 +334,9 @@ class AiPodcastClipper:
 
         print(clip_moments)
 
-        # change to higher amount in production
+        # change to higher amount in production :1
         # 3. Process clips
-        for index, moment in enumerate(clip_moments[:5]):
+        for index, moment in enumerate(clip_moments[:1]):
             if "start" in moment and "end" in moment:
                 print("Processing clip" + str(index) + " from " +
                       str(moment["start"]) + " to " + str(moment["end"]))
